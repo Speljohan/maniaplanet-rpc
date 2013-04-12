0d36edfb4693a29ea6834fccd50f55c46a4ebb97
Johan Ljungberg <johan.f.ljungberg@gmail.com>
2013-04-12 15:07:22 +0200

    Added callback support, bumped up one minor version.

--- lib/maniaplanet_rpc/maniaplanet_client.rb
+++ lib/maniaplanet_rpc/maniaplanet_client.rb
@@ -1,137 +1,165 @@
 require 'xmlrpc/client'
 require 'thread'
 
-class ManiaConnection
+module ManiaRPC
+  class ManiaConnection
 
-  include XMLRPC::ParserWriterChooseMixin
+    include XMLRPC::ParserWriterChooseMixin
 
-  def initialize(ip, port, callback)
-    @callback = callback
-    @request_handle = 0x80000000
-    @ip = ip
-    @port = port
-    @send_queue = Queue.new
-    @socket = TCPSocket.new ip, port
-    @protocol = 0
-    authenticate
-  end
-
-  def authenticate
-    header = @socket.recv(4).unpack "Vsize"
-
-    if header[0] > 64
-      raise Exception, "Wrong low-level protocol header!"
+    def initialize(ip, port, callback)
+      @callback = callback
+      @request_handle = 0x80000000
+      @ip = ip
+      @port = port
+      @send_queue = Queue.new
+      begin
+        @socket = TCPSocket.new ip, port
+      rescue
+        puts "Failed to establish a connection, is the ManiaPlanet server really running?"
+        exit
+      end
+      @protocol = 0
+      handshake
     end
 
-    handshake = @socket.recv header[0]
+    def handshake
+      header = @socket.recv(4).unpack "Vsize"
 
-    if handshake == "GBXRemote 1"
-      @protocol = 1
-    elsif handshake == "GBXRemote 2"
-      @protocol = 2
-    else
-      raise Exception, "Unknown protocol version!"
+      if header[0] > 64
+        raise Exception, "Wrong low-level protocol header!"
+      end
+
+      handshake = @socket.recv header[0]
+
+      if handshake == "GBXRemote 1"
+        @protocol = 1
+      elsif handshake == "GBXRemote 2"
+        @protocol = 2
+      else
+        raise Exception, "Unknown protocol version!"
+      end
     end
-  end
 
-  def start
-    loop do
-      sleep(1.0/24.0)
-      tick
+    def start
+      loop do
+        sleep(1.0/24.0)
+        tick
+      end
     end
-  end
 
-  def call(method, *args)
-    @request_handle = @request_handle + 1
-    @send_queue.push [@request_handle, create().methodCall(method, *args)]
-    @request_handle - 0x80000000
-  end
+    def call(method, *args)
+      @request_handle = @request_handle + 1
+      @send_queue.push [@request_handle, create().methodCall(method, *args)]
+      @request_handle - 0x80000000
+    end
 
-  def tick
-    write
-    read
-  end
+    def tick
+      write
+      read
+    end
 
-  def read
-    if IO.select([@socket], nil, nil, 0) != nil
-      if @protocol == 1
-        content = @socket.recv 4
-        if content.bytesize == 0
-          raise Exception, "Cannot read size!"
+    def read
+      if IO.select([@socket], nil, nil, 0) != nil
+        if @protocol == 1
+          content = @socket.recv 4
+          if content.bytesize == 0
+            raise Exception, "Cannot read size!"
+          end
+          result = content.unpack "Vsize"
+          size = result[0]
+          receive_handle = @request_handle
+        else
+          content = @socket.recv 8
+          if content.bytesize == 0
+            raise Exception, "Cannot read size/handle!"
+          end
+          result = content.unpack "Vsize/Vhandle"
+          size = result[0]
+          receive_handle = result[1]
         end
-        result = content.unpack "Vsize"
-        size = result[0]
-        receive_handle = @request_handle
-      else
-        content = @socket.recv 8
-        if content.bytesize == 0
-          raise Exception, "Cannot read size/handle!"
+
+        if receive_handle == 0 || size == 0
+          raise Exception, "Connection interrupted!"
         end
-        result = content.unpack "Vsize/Vhandle"
-        size = result[0]
-        receive_handle = result[1]
+
+        if size > 4096 * 1024
+          raise Exception, "Response too large!"
+        end
+
+        response = ""
+        response_length = 0
+
+        while response_length < size
+          response << @socket.recv(size - response_length)
+          response_length = response.bytesize
+        end
+
+        begin # Response
+          response = parser().parseMethodResponse(response)
+          @callback.response_map[receive_handle].call response
+        rescue Exception # Callback
+          response = parser().parseMethodCall(response)
+          @callback.parse_callback response
+        end
       end
+    end
 
-      if receive_handle == 0 || size == 0
-        raise Exception, "Connection interrupted!"
+    def write
+      while @send_queue.length > 0
+        request = @send_queue.pop
+        if @protocol == 1
+          bytes = [request[1].bytesize, request[1]].pack("Va*")
+        else
+          bytes = [request[1].bytesize, request[0], request[1]].pack("VVa*")
+        end
+        @socket.write bytes
       end
+    end
 
-      if size > 4096 * 1024
-        raise Exception, "Response too large!"
+  end
+
+  ##
+  # This class provides the interface for a ManiaPlanet Server by sending/receiving requests as well as callbacks.
+  class ManiaClient
+
+    attr_reader :connection
+    attr_accessor :response_map
+
+    def initialize(ip, port)
+      @response_map = {}
+      @callback_map = {}
+      @connection = ManiaConnection.new ip, port, self
+      Thread.new do
+        @connection.start
       end
+    end
 
-      response = ""
-      response_length = 0
+    def parse_callback(message)
+      @callback_map[message.first].call message
+      @callback_map[:all].call message
+    end
 
-      while response_length < size
-        response << @socket.recv(size - response_length)
-        response_length = response.bytesize
-      end
+    ##
+    # Call this with a block to handle a callback with the given name.
+    def on(what, &block)
+      @callback_map[what] = block
+    end
 
-      if @callback.response_map[receive_handle] == nil
-        @callback.parse_callback parser().parseMethodResponse(response)
+    ##
+    # Call this with a block to receive all callbacks. Can be used in conjunction with the on method.
+    def all(&block)
+      @callback_map[:all] = block
+    end
+
+    ##
+    # Calls an RPC method with the given name and arguments, optionally handling the result within the passed block.
+    # Note: The response is asynchronous.
+    def call(method, *args, &block)
+      if block_given?
+        @response_map[@connection.call(method, *args)] = block
       else
-        @callback.response_map[receive_handle].call parser().parseMethodResponse(response)
+        @response_map[@connection.call(method, *args)] = proc {}
       end
-    end
-  end
-
-  def write
-    while @send_queue.length > 0
-      request = @send_queue.pop
-      if @protocol == 1
-        bytes = [request[1].bytesize, request[1]].pack("Va*")
-      else
-        bytes = [request[1].bytesize, request[0], request[1]].pack("VVa*")
-      end
-      @socket.write bytes
-    end
-  end
-
-end
-
-class ManiaClient
-
-  attr_reader :connection
-  attr_accessor :response_map
-
-  def initialize(ip, port)
-    @response_map = {}
-    @connection = ManiaConnection.new ip, port, self
-    Thread.new do
-      @connection.start
-    end
-  end
-
-  def parse_callback(message)
-    # TODO: Fix
-  end
-
-  def call(method, *args, &block)
-    if block_given?
-      @response_map[@connection.call(method, *args)] = block
-    else
-      @response_map[@connection.call(method, *args)] = proc {}
     end
   end
 end
--- maniaplanet_rpc.gemspec
+++ maniaplanet_rpc.gemspec
@@ -1,9 +1,9 @@
 Gem::Specification.new do |s|
   s.name        = 'maniaplanet-rpc'
-  s.version     = '0.1.0'
-  s.date        = '2013-04-08'
+  s.version     = '0.2.0'
+  s.date        = '2013-04-12'
   s.summary     = "A library for interfacing with maniaplanet servers."
-  s.description = "maniaplanet-rpc is a library for interfacing with maniaplanet servers using their custom variant of xml-rpc"
+  s.description = "maniaplanet-rpc is a library for interfacing with maniaplanet servers using its custom variant of xml-rpc"
   s.authors     = ["Johan Ljungberg"]
   s.email       = 'johan.f.ljungberg@gmail.com'
   s.files       = ["lib/maniaplanet_rpc.rb", "lib/maniaplanet_rpc/maniaplanet_client.rb"]
